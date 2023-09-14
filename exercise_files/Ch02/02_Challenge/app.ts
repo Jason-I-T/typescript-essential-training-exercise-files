192f495ba2b35733e92b62e962605f68a057c92f
Jason-I-T <j.istejada29@gmail.com>
2023-09-14 15:39:31 -0700

    Completed the challenge for the second part

--- exercise_files/Ch02/02_Challenge/app.ts
+++ exercise_files/Ch02/02_Challenge/app.ts
@@ -1,16 +1,31 @@
-const todoItems = [
-    { id: 1, title: "Learn HTML", status: "done", completedOn: new Date("2021-09-11") },
-    { id: 2, title: "Learn TypeScript", status: "in-progress" },
-    { id: 3, title: "Write the best app in the world", status: "todo" },
+interface TodoItem {
+    id: number,
+    title: string,
+    status: TodoStatus,
+    completedOn?: Date
+}
+
+enum TodoStatus {
+    Done = "done",
+    InProgress = "in-progress",
+    Todo = "todo"
+}
+
+//var item1: TodoItem = { id: 1, title: "Learn HTML", status: "done", completedOn: new Date("2021-09-11") }
+
+const todoItems: TodoItem[] = [
+    { id: 1, title: "Learn HTML", status: TodoStatus.Done, completedOn: new Date("2021-09-11") },
+    { id: 2, title: "Learn TypeScript", status: TodoStatus.InProgress },
+    { id: 3, title: "Write the best app in the world", status: TodoStatus.Todo },
 ]
 
-function addTodoItem(todo) {
+function addTodoItem(todo: string): TodoItem {
     const id = getNextId(todoItems)
 
     const newTodo = {
         id,
         title: todo,
-        status: "todo",
+        status: TodoStatus.Todo,
     }
 
     todoItems.push(newTodo)
@@ -18,8 +33,8 @@ function addTodoItem(todo) {
     return newTodo
 }
 
-function getNextId(items) {
-    return items.reduce((max, x) => x.id > max ? x.id : max, 0) + 1
+function getNextId<T1 extends { id: number }>(items: T1[]): number {
+    return items.reduce((max, x) => x.id > max ? max: x.id , 0) + 1
 }
 
 const newTodo = addTodoItem("Buy lots of stuff with all the money we make from the app")
