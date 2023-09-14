8ef491b54757514f312b2a3ae8276215bd43c98e
Jason-I-T <j.istejada29@gmail.com>
2023-09-14 16:41:26 -0700

    Finished 4th exercise of the third part

--- exercise_files/Ch03/03_04b/src/demo.ts
+++ exercise_files/Ch03/03_04b/src/demo.ts
@@ -13,8 +13,11 @@ interface Contact {
     address: Address;
 }
 
+type Awesome = Contact["id"]
+type VerCool = Contact["address"]["postalCode"]
+
 interface ContactEvent {
-    contactId: number;
+    contactId: Contact["id"];
 }
 
 interface ContactDeletedEvent extends ContactEvent { 
@@ -34,3 +37,22 @@ interface ContactEvents {
 function getValue<T, U extends keyof T>(source: T, propertyName: U) {
     return source[propertyName];
 }
+
+// Method to handle event for contacts
+function handleEvent<T extends keyof ContactEvents>( // Define generic type
+    eventName: T, // This parameter will accept a string containing the 
+                  //name of any of the properties defined in the ContactEvents type
+    handler: (event: ContactEvents[T]) => void // This is a function that accepts a single parameter
+                                               // that matches the type of the property indicated
+                                               // by eventName
+) {
+    if(eventName === "statusChanged") {
+        // handler({ contactId: 1} as ContactDeletedEvent) // Not correct, so typescript yells
+        handler({ contactId: 1, oldStatus: "active", newStatus: "inactive"}) // Providing object that matches the
+    }                                                                         // correct event interface
+}
+
+// TypeScript knows that the type of this event is ContactStatusChangedEvent, even though it wasn't
+// specified in this call. This is because using index access on the contact events type let it figure it
+// out on its own.
+handleEvent("statusChanged", event => event)
