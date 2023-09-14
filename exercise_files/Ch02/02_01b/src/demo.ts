91a261d579198485dd2a0da20fd72693e099fa2b
Jason-I-T <j.istejada29@gmail.com>
2023-09-14 13:16:19 -0700

    exercise in first video of second section

---
+++ exercise_files/Ch02/02_01b/src/demo.ts
@@ -0,0 +1,8 @@
+var x = 5 // Type inference
+var y: string // Explicit typing
+var z: Date[] // Explicit typing
+var zz: any // Type 'any'
+var yy: string[]
+
+yy = 123 as any // Type casting
+yy = "Hey" as any
