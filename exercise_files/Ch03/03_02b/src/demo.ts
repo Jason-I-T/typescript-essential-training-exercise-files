7da94ff9d14147dfd94df8b0a1d1235c09abb663
Jason-I-T <j.istejada29@gmail.com>
2023-09-14 16:11:58 -0700

    Completed the second exercise for part three

--- exercise_files/Ch03/03_02b/src/demo.ts
+++ exercise_files/Ch03/03_02b/src/demo.ts
@@ -14,3 +14,20 @@ let primaryContact: Contact = {
     name: "Jamie Johnson",
     status: "active"
 }
+
+type ContactFields = keyof Contact
+
+// function getValue(source: Contact, propertyName: ContactFields) {
+//     return source[propertyName]
+// }
+
+// function getValue<T>(source: T, propertyName: keyof T) {
+//     return source[propertyName]
+// }
+
+function getValue<T, U extends keyof T>(source: T, propertyName: U) {
+    return source[propertyName]
+}
+
+const value = getValue(primaryContact, "status")
+const value2 = getValue({ small: 0, big: 5}, "small")
