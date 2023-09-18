2439e3c9364c628e5366cdfe85a388ee132a2b7e
Jason-I-T <j.istejada29@gmail.com>
2023-09-18 11:28:38 -0700

    Finished 3rd exercise in part5

--- exercise_files/Ch05/05_03b/src/demo.ts
+++ exercise_files/Ch05/05_03b/src/demo.ts
@@ -13,26 +13,28 @@ const currentUser = {
     }
 }
 
-function authorize(target: any, property: string, descriptor: PropertyDescriptor) {
-    const wrapped = descriptor.value
-
-    descriptor.value = function () {
-        if (!currentUser.isAuthenticated()) {
-            throw Error("User is not authenticated");
+function authorize(role: string) {
+    return function authorizeDecorator(target: any, property: string, descriptor: PropertyDescriptor) {
+        const wrapped = descriptor.value
+        descriptor.value = function () {
+            if (!currentUser.isAuthenticated()) {
+                throw Error("User is not authenticated");
+            }
+            if (!currentUser.isInRole(role)) {
+                throw Error(`User not in role ${role}`);
+            }
+            return wrapped.apply(this, arguments);
         }
-
-        return wrapped.apply(this, arguments);
     }
 }
-
 class ContactRepository {
     private contacts: Contact[] = [];
 
     @authorize("ContactViewer")
     getContactById(id: number): Contact | null {
-        if (!currentUser.isInRole("ContactViewer")) {
-            throw Error("User not authorized to execute this action");
-        }
+        // if (!currentUser.isInRole("ContactViewer")) {
+        //     throw Error("User not authorized to execute this action");
+        // }
 
         const contact = this.contacts.find(x => x.id === id);
         return contact;
